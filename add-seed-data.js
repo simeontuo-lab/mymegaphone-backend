24e844738e71d5af3be466f14d5c139b3bd18f5b
simeontuo-lab <simeon.tuo@gmail.com>
2026-05-28 15:24:28 -0700

    1

--- add-seed-data.js
+++ add-seed-data.js
@@ -6,7 +6,7 @@ async function addSeedData () {
     const uri = process.env.MONGO_URI
     const client = new MongoClient(uri)
     await client.connect()
-    const db = client.db("megaphone")
+    const db = client.db("mymegaphone")
     const result = await db.collection("posts").insertMany(seedData)
     console.log(`Inserted ${result.insertedCount} posts.`)
     await client.close()
